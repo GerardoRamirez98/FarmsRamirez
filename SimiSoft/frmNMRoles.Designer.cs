e6386e68d0237e6d3592c8912202be8dc9a3377a
JUAN GERARDO TOVAR RAMIREZ <LRS17110289@purisima.tecnm.mx>
2023-05-23 11:19:52 -0600

    todo

---
+++ SimiSoft/frmNMRoles.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace SimiSoft
+{
+    partial class frmNMRoles
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDescripcion = new DevExpress.XtraEditors.TextEdit();
+            this.lblDescripcion = new DevExpress.XtraEditors.LabelControl();
+            this.btnCancelar = new DevExpress.XtraEditors.SimpleButton();
+            this.btnGuardar = new DevExpress.XtraEditors.SimpleButton();
+            this.txtID = new DevExpress.XtraEditors.TextEdit();
+            this.lblID = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDescripcion.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtID.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(81, 36);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(201, 20);
+            this.txtDescripcion.TabIndex = 0;
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.Location = new System.Drawing.Point(12, 39);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(58, 13);
+            this.lblDescripcion.TabIndex = 1;
+            this.lblDescripcion.Text = "Descripción:";
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancelar.Location = new System.Drawing.Point(126, 62);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 2;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(207, 62);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 3;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(81, 10);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(101, 20);
+            this.txtID.TabIndex = 4;
+            // 
+            // lblID
+            // 
+            this.lblID.Location = new System.Drawing.Point(12, 13);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(15, 13);
+            this.lblID.TabIndex = 5;
+            this.lblID.Text = "ID:";
+            // 
+            // frmNMRoles
+            // 
+            this.AcceptButton = this.btnGuardar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancelar;
+            this.ClientSize = new System.Drawing.Size(294, 116);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.txtDescripcion);
+            this.Name = "frmNMRoles";
+            this.Text = "frmNMRoles";
+            ((System.ComponentModel.ISupportInitialize)(this.txtDescripcion.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtID.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.TextEdit txtDescripcion;
+        private DevExpress.XtraEditors.LabelControl lblDescripcion;
+        private DevExpress.XtraEditors.SimpleButton btnCancelar;
+        private DevExpress.XtraEditors.SimpleButton btnGuardar;
+        private DevExpress.XtraEditors.TextEdit txtID;
+        private DevExpress.XtraEditors.LabelControl lblID;
+    }
+}
---
+++ SimiSoft/frmNMRoles.cs
@@ -0,0 +1,94 @@
+﻿using DevExpress.XtraEditors;
+using FarmsRamirezBML;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimiSoft
+{
+    public partial class frmNMRoles : DevExpress.XtraEditors.XtraForm
+    {
+        private Roles roles;
+        public frmNMRoles()
+        {
+            InitializeComponent();
+            txtID.Enabled = false;
+        }
+
+        public frmNMRoles(int rolID)
+        {
+            InitializeComponent();
+            roles = new Roles
+            {
+                rolID = rolID
+            }.GetById();
+            txtID.Text = roles.rolID.ToString();
+            txtDescripcion.Text = roles.descripcion;
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (Validar())
+            {
+                if (roles == null)
+                {
+                    if (new Roles
+                    {
+                        descripcion = txtDescripcion.Text
+                    }.Add() > 0)
+                    {
+                        XtraMessageBox.Show("Rol insertado correctamente", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        this.Close();
+                    }
+                    else
+                    {
+                        XtraMessageBox.Show("Ocurrio un error en la insercion", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        this.Close();
+                    }
+                }
+                else
+                {
+                    roles.descripcion = txtDescripcion.Text;
+
+                    if (roles.Update() > 0)
+                    {
+                        XtraMessageBox.Show("Rol modificado correctamente", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        this.Close();
+                    }
+                    else
+                    {
+                        XtraMessageBox.Show("Ocurrio un error en la modificacion", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        this.Close();
+                    }
+                }
+            }
+        }
+
+        private bool Validar()
+        {
+            var ban = false;
+            //descripcion
+            if (string.IsNullOrEmpty(txtDescripcion.Text))
+            {
+                txtDescripcion.ErrorText = "Ingrese la descripcion del rol";
+                if (!ban)
+                {
+                    txtDescripcion.Focus();
+                    ban = true;
+                }
+            }
+            return !ban;
+        }
+    }
+}
